a2bd5ce7b791fa8e0fe856194ffb31f15f48d64f
Semen <lbdw.smn@gmail.com>
2023-10-30 13:46:49 +0300

    module.scss for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,28 @@
-import './header.scss'
+import style from './header.module.scss'
 
 const Header = () => {
     return ( 
-        <header className='header'>
-            <div className="header__logo">
-                <img className='header__image' src="/icon/logo.svg" alt="logo" />
+        <header className={style.header}>
+            <div className={style.header__logo}>
+                <img className={style.header__image} src="/icon/logo.svg" alt="logo" />
                 <div className="header__text">
-                    <h3 className="text-title">REACT SNEAKERS</h3>
-                    <p className="text-description">Магазин лучших кроссовок</p>
+                    <h3 className={style.text__title}>REACT SNEAKERS</h3>
+                    <p className={style.text__description}>Магазин лучших кроссовок</p>
                 </div>
             </div>
-            <nav className="header__navigation">
-                <ul className='list__navigation'>
-                    <li className='list__navigation-item'>
-                        <img className='list__navigation-image' src="/icon/cart.svg" alt="Cart" />
-                        <p className='list__navigation-text text-cart'>1205 руб.</p>
+            <nav className={style.navigation}>
+                <ul className={style.list__navigation}>
+                    <li className={style.list__item}>
+                        <img className={style.list__image} src="/icon/cart.svg" alt="Cart" />
+                        <p className={[style.list__text, style.text__cart].join(' ')}>1205 руб.</p>
                     </li>
-                    <li className='list__navigation-item'>
-                        <img className='list__navigation-image' src="/icon/love-sneakers.svg" alt="Love sneakers" />
-                        <p className='list__navigation-text'>Закладки</p>
+                    <li className={style.list__item}>
+                        <img className={style.list__image} src="/icon/love-sneakers.svg" alt="Love sneakers" />
+                        <p className={style.list__text}>Закладки</p>
                     </li>
-                    <li className='list__navigation-item'>
-                        <img className='list__navigation-image' src="/icon/user.svg" alt="User" />
-                        <p className='list__navigation-text'>Профиль</p>
+                    <li className={style.list__item}>
+                        <img className={style.list__image} src="/icon/user.svg" alt="User" />
+                        <p className={style.list__text}>Профиль</p>
                     </li>
                 </ul>
             </nav>
